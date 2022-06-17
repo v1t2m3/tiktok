667ab7c4adb3d90b0399cb5bdaaefec73054a4ac
v1t2m3 <nguyenvantamdn@gmail.com>
2022-06-17 11:45:43 +0700

    step5

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,4 +1,4 @@
-import Header from "./Header";
+import Header from "../components/Header";
 import SideBar from "./SideBar";
 function DefaultLayout({ children }) {
   return (
--- src/components/Layout/HeaderOnly/Header/index.js
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h2>HeaderOnly</h2>;
-}
-
-export default Header;
