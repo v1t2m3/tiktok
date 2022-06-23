2e8fd907510c44d449a489b3b316e14cd435bcd6
v1t2m3 <nguyenvantamdn@gmail.com>
2022-06-23 23:47:50 +0700

    step16

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,25 +1,20 @@
-import PropTypes from "prop-types";
-import Button from "../../Button";
-import classNames from "classnames/bind";
-import styles from "./Menu.module.scss";
+import PropTypes from 'prop-types';
+import Button from '../../Button';
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
 function MenuItem({ data, onClick }) {
-  const classes = cx("menu-item", { separte: data.separte });
-  return (
-    <Button
-      className={classes}
-      to={data.to}
-      leftIcon={data.icon}
-      onClick={onClick}
-    >
-      {data.title}
-    </Button>
-  );
+	const classes = cx('menu-item', { separate: data.separate });
+	return (
+		<Button className={classes} to={data.to} leftIcon={data.icon} onClick={onClick}>
+			{data.title}
+		</Button>
+	);
 }
 MenuItem.propTypes = {
-  data: PropTypes.object.isRequired,
-  onClick: PropTypes.func,
+	data: PropTypes.object.isRequired,
+	onClick: PropTypes.func
 };
 export default MenuItem;
