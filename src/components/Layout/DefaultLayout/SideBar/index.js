a5924ada0daf3c4c5d1beaf100df02e34d94dd49
v1t2m3 <nguyenvantamdn@gmail.com>
2022-06-27 06:07:02 +0700

    step17

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,10 +1,27 @@
-import React from "react";
-import styles from "./SideBar.module.scss";
-import classNames from "classnames/bind";
+import React from 'react';
+import Styles from './SideBar.module.scss';
+import classNames from 'classnames/bind';
+import Button from '../../../Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(Styles);
 function SideBar() {
-  return <aside className={cx("wrapper")}>SideBar</aside>;
+	return (
+		<aside className={cx('wrapper')}>
+			<div className={cx('menu')}>
+				<Button className={cx('menu-item')} to="/forYou" leftIcon={<FontAwesomeIcon icon={faHouse} />}>
+					For You
+				</Button>
+				<Button className={cx('menu-item')} to="/following" leftIcon={<FontAwesomeIcon icon={faUserGroup} />}>
+					Following
+				</Button>
+				<Button className={cx('menu-item')} to="/live" leftIcon={<FontAwesomeIcon icon={faVideo} />}>
+					Live
+				</Button>
+			</div>
+		</aside>
+	);
 }
 
 export default SideBar;
