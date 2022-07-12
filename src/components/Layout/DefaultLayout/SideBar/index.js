174e4dab61784a6362113faa1185d4499a996148
v1t2m3 <nguyenvantamdn@gmail.com>
2022-07-12 15:47:56 +0700

    step23

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,73 +1,90 @@
-import React, { useEffect, useState } from 'react';
-import Styles from './SideBar.module.scss';
-import classNames from 'classnames/bind';
-import Button from '../../../Button';
-import AccountItem from '../../components/Header/SearchAccountItem';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import Styles from "./SideBar.module.scss";
+import classNames from "classnames/bind";
+import Button from "../../../Button";
+import AccountItem from "../../components/Header/SearchAccountItem";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUserGroup,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(Styles);
 
 function SideBar() {
-	const [ accountInfo, setAccountInfo ] = useState({});
-	useEffect(() => {
-		axios
-			.get(`http://localhost:3004/suggested_acc`)
-			.then((res) => {
-				setAccountInfo(res.data);
-			})
-			.catch((error) => console.log(error));
-	}, []);
-	console.log(accountInfo[1]);
-	return (
-		<aside className={cx('wrapper')}>
-			<div className={cx('menu')}>
-				<Button className={cx('menu-item')} to="/forYou" leftIcon={<FontAwesomeIcon icon={faHouse} />}>
-					For You
-				</Button>
-				<Button className={cx('menu-item')} to="/following" leftIcon={<FontAwesomeIcon icon={faUserGroup} />}>
-					Following
-				</Button>
-				<Button className={cx('menu-item')} to="/live" leftIcon={<FontAwesomeIcon icon={faVideo} />}>
-					LIVE
-				</Button>
-			</div>
-			<div className={cx('suggested-accounts')}>
-				<p>Suggested accounts</p>
-				<AccountItem info={accountInfo[0]} />
-				<Button to="/seeall" className={cx('seeall-btn')}>
-					See all
-				</Button>
-			</div>
-			<div className={cx('discovery')}>
-				<Button className={cx('hashtag')} rounded>
-					#giaitri
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					#amnhac
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					#thegioidienanh
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					#critianoRonaldo
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					# Việt Nam hỡi!
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					# Không ai chung tình được mãi
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					# Nụ cười tuổi 18
-				</Button>
-				<Button className={cx('hashtag')} rounded>
-					# Anh thanh niên
-				</Button>
-			</div>
-		</aside>
-	);
+  const [suggestedList, setSuggestedList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3400/suggested_acc").then((response) => {
+      setSuggestedList(response.data);
+    });
+  }, []);
+  console.log(suggestedList);
+
+  return (
+    <aside className={cx("wrapper")}>
+      <div className={cx("menu")}>
+        <Button
+          className={cx("menu-item")}
+          to="/forYou"
+          leftIcon={<FontAwesomeIcon icon={faHouse} />}
+        >
+          For You
+        </Button>
+        <Button
+          className={cx("menu-item")}
+          to="/following"
+          leftIcon={<FontAwesomeIcon icon={faUserGroup} />}
+        >
+          Following
+        </Button>
+        <Button
+          className={cx("menu-item")}
+          to="/live"
+          leftIcon={<FontAwesomeIcon icon={faVideo} />}
+        >
+          LIVE
+        </Button>
+      </div>
+      <div className={cx("suggested-accounts")}>
+        <p>Suggested accounts</p>
+        {suggestedList.map((item) => (
+          <AccountItem key={item.id} info={item} />
+        ))}
+        <Button to="/seeall" className={cx("seeall-btn")}>
+          See all
+        </Button>
+      </div>
+      <div className={cx("discovery")}>
+        <Button className={cx("hashtag")} rounded>
+          #giaitri
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          #amnhac
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          #thegioidienanh
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          #critianoRonaldo
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          # Việt Nam hỡi!
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          # Không ai chung tình được mãi
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          # Nụ cười tuổi 18
+        </Button>
+        <Button className={cx("hashtag")} rounded>
+          # Anh thanh niên
+        </Button>
+      </div>
+    </aside>
+  );
 }
 
 export default SideBar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,204 +1,200 @@
-import React, { useEffect, useState } from 'react';
-import classNames from 'classnames/bind';
-import HeadlessTippy from '@tippyjs/react/headless';
-import Tippy from '@tippyjs/react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useDispatch, useSelector } from 'react-redux';
-import { logInPage } from '../../../../redux/actions';
-import { loginSelector } from '../../../../redux/selector';
+import React, { useEffect, useState } from "react";
+import classNames from "classnames/bind";
+import HeadlessTippy from "@tippyjs/react/headless";
+import Tippy from "@tippyjs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch, useSelector } from "react-redux";
+import { logInPage } from "../../../../redux/actions";
+import { loginSelector } from "../../../../redux/selector";
 
-import 'tippy.js/dist/tippy.css';
-import Button from '../../../Button';
-import styles from './Header.module.scss';
-import images from '../../../../assets/images';
+import "tippy.js/dist/tippy.css";
+import Button from "../../../Button";
+import styles from "./Header.module.scss";
+import images from "../../../../assets/images";
 import {
-	faChain,
-	faCircleXmark,
-	faCloudUploadAlt,
-	faCoins,
-	faEarthAsia,
-	faEllipsisVertical,
-	faKeyboard,
-	faMagnifyingGlass,
-	faMessage,
-	faPerson,
-	faQuestion,
-	faSignOut,
-	faSpinner,
-	faUserGear
-} from '@fortawesome/free-solid-svg-icons';
-import { Wrapper as PopperWrapper } from '../../../Popper';
-import AccountItem from './SearchAccountItem';
-import Menu from '../../../Popper/Menu';
-import { faSteamSymbol } from '@fortawesome/free-brands-svg-icons';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+  faChain,
+  faCircleXmark,
+  faCloudUploadAlt,
+  faCoins,
+  faEarthAsia,
+  faEllipsisVertical,
+  faKeyboard,
+  faMagnifyingGlass,
+  faMessage,
+  faPerson,
+  faQuestion,
+  faSignOut,
+  faSpinner,
+  faUserGear,
+} from "@fortawesome/free-solid-svg-icons";
+import { Wrapper as PopperWrapper } from "../../../Popper";
+import AccountItem from "./SearchAccountItem";
+import Menu from "../../../Popper/Menu";
+import { faSteamSymbol } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 const handleMenuChange = (menuItem) => {
-	switch (menuItem.type) {
-		case 'language':
-			// fcc
-			break;
-		default:
-	}
+  switch (menuItem.type) {
+    case "language":
+      // fcc
+      break;
+    default:
+  }
 };
 
 const MENU_ITEMS = [
-	{
-		icon: <FontAwesomeIcon icon={faEarthAsia} />,
-		title: 'English',
-		children: {
-			title: 'Language',
-			data: [
-				{
-					code: 'En',
-					title: 'English'
-				},
-				{
-					code: 'Fc',
-					title: 'France'
-				},
-				{
-					code: 'Vi',
-					title: 'Vietnam'
-				}
-			]
-		}
-	},
-	{
-		icon: <FontAwesomeIcon icon={faQuestion} />,
-		title: 'Feeback and Help',
-		to: '/feedback_page'
-	},
-	{
-		icon: <FontAwesomeIcon icon={faKeyboard} />,
-		title: 'Keyboard Shorcut'
-	}
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: "English",
+    children: {
+      title: "Language",
+      data: [
+        {
+          code: "En",
+          title: "English",
+        },
+        {
+          code: "Fc",
+          title: "France",
+        },
+        {
+          code: "Vi",
+          title: "Vietnam",
+        },
+      ],
+    },
+  },
+  {
+    icon: <FontAwesomeIcon icon={faQuestion} />,
+    title: "Feeback and Help",
+    to: "/feedback_page",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: "Keyboard Shorcut",
+  },
 ];
 const userMenu = [
-	{
-		icon: <FontAwesomeIcon icon={faUser} />,
-		title: 'View Profile',
-		to: '/Profile_page'
-	},
-	{
-		icon: <FontAwesomeIcon icon={faCoins} />,
-		title: 'Get coints',
-		to: '/getCoins'
-	},
-	{
-		icon: <FontAwesomeIcon icon={faUserGear} />,
-		title: 'Setting',
-		to: '/setting'
-	},
-	...MENU_ITEMS,
-	{
-		icon: <FontAwesomeIcon icon={faSignOut} />,
-		title: 'Log out',
-		to: '/login',
-		separate: true
-	}
+  {
+    icon: <FontAwesomeIcon icon={faUser} />,
+    title: "View Profile",
+    to: "/Profile_page",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCoins} />,
+    title: "Get coints",
+    to: "/getCoins",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faUserGear} />,
+    title: "Setting",
+    to: "/setting",
+  },
+  ...MENU_ITEMS,
+  {
+    icon: <FontAwesomeIcon icon={faSignOut} />,
+    title: "Log out",
+    to: "/login",
+    separate: true,
+  },
 ];
 const cx = classNames.bind(styles);
 function Header() {
-	const [ searchResult, setSearchResult ] = useState([]);
-	useEffect(() => {
-		setTimeout(() => {
-			setSearchResult([]);
-		}, 3000);
-	}, []);
-	const dispatch = useDispatch();
-	const currentUser = useSelector(loginSelector);
-	console.log('currentUser:', currentUser);
-	return (
-		<header className={cx('wrapper')}>
-			<div className={cx('inner')}>
-				<div className={cx('logo')}>
-					<img src={images.logo} alt="TIKTOK" />
-				</div>
-				<HeadlessTippy
-					interactive
-					visibles
-					render={(attrs) => (
-						<div className={cx('search-tippy')} tabIndex="-1" {...attrs}>
-							<PopperWrapper>
-								<h4 className={cx('search-title')}>Accounts</h4>
-								<AccountItem
-									name="tamnv"
-									fullname="nguyenvantam"
-									avatar={require('../../../../assets/avatar/avatar1.jpeg')}
-								/>
-								<AccountItem
-									name="hoahoctro"
-									fullname="Hoa học trò"
-									avatar={require('../../../../assets/avatar/avatar2.jpeg')}
-								/>
-								<AccountItem
-									name="Mad_makita"
-									fullname="Mad and makita"
-									avatar={require('../../../../assets/avatar/avatar3.jpeg')}
-								/>
-							</PopperWrapper>
-						</div>
-					)}
-				>
-					<div className={cx('search')}>
-						<input placeholder="Search accounts and videos" spellCheck="false" />
-						<button className={cx('clear')}>
-							<FontAwesomeIcon icon={faCircleXmark} />
-						</button>
-						<FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-						<div />
-						<button className={cx('search-btn')}>
-							<FontAwesomeIcon icon={faMagnifyingGlass} />
-						</button>
-					</div>
-				</HeadlessTippy>
-				<div className={cx('action')}>
-					{currentUser ? (
-						<div>
-							<Tippy delay={300} content="Upload video" placement="bottom">
-								<button className={cx('action-btn')}>
-									<FontAwesomeIcon icon={faCloudUploadAlt} />
-								</button>
-							</Tippy>
-							<Tippy delay={300} content="Messenger" placement="bottom">
-								<button className={cx('action-btn')}>
-									<FontAwesomeIcon icon={faMessage} />
-								</button>
-							</Tippy>
-						</div>
-					) : (
-						<div className={cx('action-user')}>
-							<Button text to="/upload">
-								Up load
-							</Button>
-							<Button primary to="/login">
-								Login
-							</Button>
-						</div>
-					)}
-					<Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
-						{currentUser ? (
-							<div>
-								<img
-									src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-									className={cx('user-avatar')}
-									alt="Nguyen Van Tam"
-								/>
-							</div>
-						) : (
-							<div>
-								<button className={cx('menu-btn')}>
-									<FontAwesomeIcon icon={faEllipsisVertical} />
-								</button>
-							</div>
-						)}
-					</Menu>
-				</div>
-				<div />
-			</div>
-		</header>
-	);
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 3000);
+  }, []);
+  const dispatch = useDispatch();
+  const currentUser = useSelector(loginSelector);
+  console.log("currentUser:", currentUser);
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="TIKTOK" />
+        </div>
+        <HeadlessTippy
+          interactive
+          //   visible
+          render={(attrs) => (
+            <div className={cx("search-tippy")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Accounts</h4>
+                {/* <AccountItem
+                  name="tamnv"
+                  fullname="nguyenvantam"
+                  avatar={require("../../../../assets/avatar/avatar1.jpeg")}
+                /> */}
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input
+              placeholder="Search accounts and videos"
+              spellCheck="false"
+            />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+            <div />
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </HeadlessTippy>
+        <div className={cx("action")}>
+          {currentUser ? (
+            <div>
+              <Tippy delay={300} content="Upload video" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <FontAwesomeIcon icon={faCloudUploadAlt} />
+                </button>
+              </Tippy>
+              <Tippy delay={300} content="Messenger" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <FontAwesomeIcon icon={faMessage} />
+                </button>
+              </Tippy>
+            </div>
+          ) : (
+            <div className={cx("action-user")}>
+              <Button text to="/upload">
+                Up load
+              </Button>
+              <Button primary to="/login">
+                Login
+              </Button>
+            </div>
+          )}
+          <Menu
+            items={currentUser ? userMenu : MENU_ITEMS}
+            onChange={handleMenuChange}
+          >
+            {currentUser ? (
+              <div>
+                <img
+                  src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  className={cx("user-avatar")}
+                  alt="Nguyen Van Tam"
+                />
+              </div>
+            ) : (
+              <div>
+                <button className={cx("menu-btn")}>
+                  <FontAwesomeIcon icon={faEllipsisVertical} />
+                </button>
+              </div>
+            )}
+          </Menu>
+        </div>
+        <div />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
